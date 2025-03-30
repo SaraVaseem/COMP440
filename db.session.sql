dc5d8a3e85c042ed3f46d8e7b56061953e8cf77a
Sarasormad <saramadjdi@gmail.com>
2025-03-29 17:31:51 -0700

    created table listings and the auto increment to the  ID

--- db.session.sql
+++ db.session.sql
@@ -1,1 +1,9 @@
 select * from user
+
+create table listings (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    feature VARCHAR(255) NOT NULL,
+    price int NOT NULL
+);
